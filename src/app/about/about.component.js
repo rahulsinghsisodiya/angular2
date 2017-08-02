a28d850fffb74b9db4d3f845840b112dbab40e60
Rahul Singh <rahul.singh@cgt.co.in>
2017-08-02 19:23:48 +0530

    first

--- src/app/about/about.component.js
+++ src/app/about/about.component.js
@@ -5,17 +5,26 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 var core_1 = require("@angular/core");
+var router_1 = require("@angular/router");
 var AppAbout = (function () {
-    function AppAbout() {
+    function AppAbout(_router) {
+        this._router = _router;
     }
+    AppAbout.prototype.onBack = function () {
+        this._router.navigate(['']);
+    };
     return AppAbout;
 }());
 AppAbout = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: 'about',
-    })
+        template: 'Inventory <a class = "button" (click) = "onBack()">Back to Products</a>'
+    }),
+    __metadata("design:paramtypes", [router_1.Router])
 ], AppAbout);
 exports.AppAbout = AppAbout;
 //# sourceMappingURL=about.component.js.map
--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import { Router }  from '@angular/router';
 @Component({
   selector: 'my-app',
-  template: 'about' ,
+   template: 'Inventory <a class = "button" (click) = "onBack()">Back to Products</a>' 
 })
+
 export class AppAbout  { 
 
+constructor(private _router: Router){} 
 
+   onBack(): void { 
+      this._router.navigate(['']); 
+   } 
  }
